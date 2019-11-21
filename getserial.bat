5cfedeb330d170be053ed51e778a00443c18e860
devilinside71 <devilinside71@users.noreply.github.com>
2019-11-21 07:00:46 +0100

    Formatting

--- getserial.bat
+++ getserial.bat
@@ -4,16 +4,16 @@ wmic nicconfig get Description, MACAddress /format:csv | find ":" >> wniclist.tx
 wmic bios get Name, Manufacturer, SerialNumber /format:csv | find "Ver" >> wbioslist.txt
 wmic OS LIST BRIEF /format:csv  | find "WINDOWS" >> woslist.txt
 FOR /F "tokens=* USEBACKQ" %%F IN (`where /r "c:\Program Files" ospp.vbs`) DO (
-SET ospppath=%%F
+SET ospppath64=%%F
 )
 FOR /F "tokens=* USEBACKQ" %%F IN (`where /r "c:\Program Files (x86)" ospp.vbs`) DO (
-SET ospppath2=%%F
+SET ospppath32=%%F
 )
 wmic bios get Name, Manufacturer, SerialNumber /format:csv | find "Ver" >> wofficelist.txt
 echo Office 64 bit: >> wofficelist.txt
-cscript "%ospppath%" /dstatus | findstr "PRODUCT LICENSE key:" >> wofficelist.txt
+cscript "%ospppath64%" /dstatus | findstr "PRODUCT LICENSE key:" >> wofficelist.txt
 echo Office 32 bit: >> wofficelist.txt
-cscript "%ospppath2%" /dstatus | findstr "PRODUCT LICENSE key:" >> wofficelist.txt
+cscript "%ospppath32%" /dstatus | findstr "PRODUCT LICENSE key:" >> wofficelist.txt
 echo ---------------------------------------------------- >> wofficelist.txt
 wmic bios get Name, Manufacturer, SerialNumber /format:csv | find "Ver"
 echo FINISHED
